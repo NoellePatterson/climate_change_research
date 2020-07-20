921cd0de713d9fb16d33aba073fa5492c16cc105
Noelle Patterson <noellekpatterson@gmail.com>
2020-07-20 11:09:38 -0700

    add 10 90 percentile bands and improve code

--- hydrograph.py
+++ hydrograph.py
@@ -2,6 +2,29 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def define_fill_points(year_type, percent, spmed_y, sp_rocmed_y):
+    ws_x = year_type.loc['Wet_Tim_'+percent]
+    pre_sp_x = year_type.loc['SP_Tim_'+percent]-60
+    sp_x = year_type.loc['SP_Tim_'+percent]
+    sp_roc_x = year_type.loc['SP_Tim_'+percent]+30
+    ds_x = year_type.loc['DS_Tim_'+percent]
+
+    # all 25th percentile fill y vals
+    fa_y = year_type.loc['FA_Mag_'+percent]
+    ds_y = year_type.loc['DS_Mag_50_'+percent]
+    ws_y = year_type.loc['Wet_BFL_Mag_50_'+percent]
+    sp_y = year_type.loc['SP_Mag_'+percent]
+    sp_roc_y = sp_rocmed_y - (spmed_y - sp_y)
+    # if end of spring recession drops too low based on original magnitude settings, reassign magnitude to dry season value
+    if int(percent) < 50:
+        if sp_roc_y < ds_y:
+            sp_roc_y = ds_y
+    # if fill value is greater than median
+    elif int(percent) > 50:
+        sp_roc_y = sp_rocmed_y + (sp_y - spmed_y)
+
+    return ws_x, pre_sp_x, sp_x, sp_roc_x, ds_x, fa_y, ds_y, ws_y, sp_y, sp_roc_y
+
 '''
 Plot hydrographs with FFC metric overlays for DWR data
 '''
@@ -27,72 +50,89 @@ def hydrograph(ffc_data, rh_data):
     
     ffc_vals = pd.DataFrame(columns = ['all_years', 'dry', 'mod', 'wet'])
     metrics = ['FA_Tim', 'FA_Mag', 'FA_Dur', 'Wet_Tim', 'Wet_BFL_Mag_50', 'Peak_5', 'Peak_2','Peak_Dur_5','Peak_Dur_2','SP_Tim', 'SP_Mag', 'SP_ROC', 'DS_Tim', 'DS_Mag_50']
-    for metric in metrics:
-        ffc_vals.loc[metric+'_med', 'all_years'] = control_transpose[metric].median() 
-        ffc_vals.loc[metric+'_med', 'dry'] = dry_q[metric].median() 
-        ffc_vals.loc[metric+'_med', 'mod'] = mod_q[metric].median()  
-        ffc_vals.loc[metric+'_med', 'wet'] = wet_q[metric].median()
-    for metric in metrics:
-        ffc_vals.loc[metric+'_25', 'all_years'] = control_transpose[metric].quantile(.25) 
-        ffc_vals.loc[metric+'_25', 'dry'] = dry_q[metric].quantile(.25) 
-        ffc_vals.loc[metric+'_25', 'mod'] = mod_q[metric].quantile(.25) 
-        ffc_vals.loc[metric+'_25', 'wet'] = wet_q[metric].quantile(.25)
-    for metric in metrics:
-        ffc_vals.loc[metric+'_75', 'all_years'] = control_transpose[metric].quantile(.75) 
-        ffc_vals.loc[metric+'_75', 'dry'] = dry_q[metric].quantile(.75) 
-        ffc_vals.loc[metric+'_75', 'mod'] = mod_q[metric].quantile(.75) 
-        ffc_vals.loc[metric+'_75', 'wet'] = wet_q[metric].quantile(.75)
-
+    percentiles = [['_10', .10], ['_25', .25], ['_med', .5], ['_75', .75], ['_90', .9]]
+    for label, quantile in percentiles:
+        for metric in metrics:
+            ffc_vals.loc[metric+label, 'all_years'] = control_transpose[metric].quantile(quantile) 
+            ffc_vals.loc[metric+label, 'dry'] = dry_q[metric].quantile(quantile) 
+            ffc_vals.loc[metric+label, 'mod'] = mod_q[metric].quantile(quantile) 
+            ffc_vals.loc[metric+label, 'wet'] = wet_q[metric].quantile(quantile)
+    
     # 3. plot out hydrograph with 25/75 lines
 
-    # Find the control plotting data from the list of simulations
+    ################################## Option 1: DRH style background ####################################################
+    # # Find the control plotting data from the list of simulations
+    # for index, sim in enumerate(rh_data):
+    #     if sim['name'] == 'DT0DP1':
+    #         control_index = index
+    
+    # plot_data = rh_data[control_index]
+    # percentiles = [10, 25, 50, 75, 90]
+    # percentile_keys = ['ten', 'twenty_five', 'fifty', 'seventy_five', 'ninety']
+
+    # rh = {}
+    # for index, percentile in enumerate(percentile_keys):
+    #     rh[percentile] = []
+    # for row_index, _ in enumerate(plot_data['data'].iloc[:,0]): # loop through each row, 366 total
+    #     # loop through all five percentiles
+    #     for index, percentile in enumerate(percentiles): 
+    #         # calc flow percentiles across all years for each row of flow matrix
+    #         flow_row = pd.to_numeric(plot_data['data'].iloc[row_index, :], errors='coerce')
+    #         rh[percentile_keys[index]].append(np.nanpercentile(flow_row, percentile))
+        
+    # plt.rc('ytick', labelsize=5) 
+    # plt.subplot()
+    # name = plot_data['name']
+    # # make plot
+
+    # x = np.arange(0,366,1)
+    # ax = plt.plot(rh['ten'], color = 'darkgrey', label = "10%") # original color is navy
+    # # plt.plot(rh['twenty_five'], color = 'blue', label = "25%")
+    # plt.plot(rh['fifty'], color='red', label = "50%")
+    # # plt.plot(rh['seventy_five'], color = 'blue', label = "75%")
+    # plt.plot(rh['ninety'], color = 'darkgrey', label = "90%") # original color is navy
+    # # plt.fill_between(x, rh['ten'], rh['twenty_five'], color='powderblue')
+    # # plt.fill_between(x, rh['twenty_five'], rh['fifty'], color='powderblue')
+    # # plt.fill_between(x, rh['fifty'], rh['seventy_five'], color='powderblue')
+    # # plt.fill_between(x, rh['seventy_five'], rh['ninety'], color='powderblue')
+    # plt.fill_between(x, rh['ten'], rh['fifty'], color='lightgrey')
+    # plt.fill_between(x, rh['fifty'], rh['ninety'], color='lightgrey')
+
+    # plt.title('Control - {} - Dry flow years'.format(name), size=9)
+    # plt.ylabel('Flow (cfs)')
+    # tick_spacing = [0, 30.5, 61, 91.5, 122, 152.5, 183, 213.5, 244, 274.5, 305, 335.5]
+    # tick_labels = ['Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+    # plt.xticks(tick_spacing, tick_labels)
+    # plt.tick_params(axis='y', which='major', pad=1)
+    # plt.axhline(y=0, ls=':', color='darkgrey')
+    ###################################################################################################################
+
+    ################################## Option 2: Yearly trace 'spaghetti plots ########################################
     for index, sim in enumerate(rh_data):
         if sim['name'] == 'DT0DP1':
             control_index = index
-    
-    plot_data = rh_data[control_index]
-    percentiles = [10, 25, 50, 75, 90]
-    percentile_keys = ['ten', 'twenty_five', 'fifty', 'seventy_five', 'ninety']
-
-    rh = {}
-    for index, percentile in enumerate(percentile_keys):
-        rh[percentile] = []
-    for row_index, _ in enumerate(plot_data['data'].iloc[:,0]): # loop through each row, 366 total
-        # loop through all five percentiles
-        for index, percentile in enumerate(percentiles): 
-            # calc flow percentiles across all years for each row of flow matrix
-            flow_row = pd.to_numeric(plot_data['data'].iloc[row_index, :], errors='coerce')
-            rh[percentile_keys[index]].append(np.nanpercentile(flow_row, percentile))
-        
-    plt.rc('ytick', labelsize=5) 
+            name = sim['name']
+    plot_data = rh_data[control_index]['data']
+    plt.rc('ytick') 
     plt.subplot()
-    name = plot_data['name']
-    # make plot
-
     x = np.arange(0,366,1)
-    ax = plt.plot(rh['ten'], color = 'darkgrey', label = "10%") # original color is navy
-    # plt.plot(rh['twenty_five'], color = 'blue', label = "25%")
-    plt.plot(rh['fifty'], color='red', label = "50%")
-    # plt.plot(rh['seventy_five'], color = 'blue', label = "75%")
-    plt.plot(rh['ninety'], color = 'darkgrey', label = "90%") # original color is navy
-    # plt.fill_between(x, rh['ten'], rh['twenty_five'], color='powderblue')
-    # plt.fill_between(x, rh['twenty_five'], rh['fifty'], color='powderblue')
-    # plt.fill_between(x, rh['fifty'], rh['seventy_five'], color='powderblue')
-    # plt.fill_between(x, rh['seventy_five'], rh['ninety'], color='powderblue')
-    plt.fill_between(x, rh['ten'], rh['fifty'], color='lightgrey')
-    plt.fill_between(x, rh['fifty'], rh['ninety'], color='lightgrey')
-
-    plt.title('Control - {} - Dry flow years'.format(name), size=9)
-    plt.ylabel('Flow (cfs)')
+    greyscale = np.linspace(.35, .92, 100)
+    # import pdb; pdb.set_trace()
+    # make plot
+    for index, flow in enumerate(plot_data):
+        y_plot = pd.to_numeric(plot_data.iloc[:,index], errors='coerce')
+        plt.plot(y_plot, linewidth=.6, c=str(greyscale[index]), zorder=1)
+    plt.title('Control - {} - All flow years'.format(name), size=9)
+    plt.ylabel('Flow (cfs)') 
     tick_spacing = [0, 30.5, 61, 91.5, 122, 152.5, 183, 213.5, 244, 274.5, 305, 335.5]
     tick_labels = ['Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
     plt.xticks(tick_spacing, tick_labels)
-    plt.tick_params(axis='y', which='major', pad=1)
-    plt.axhline(y=0, ls=':', color='darkgrey')
-    
 
+    ###################################################################################################################
+    
     # 4. Plot out boxes based on FFC metric vals. 
-    year_type = ffc_vals['dry']
+    years = 'all_years'
+    year_type = ffc_vals[years]
 
     # Fall pulse component box
     # fall_x = [year_type.loc['FA_Tim_25'], year_type.loc['FA_Tim_75'], year_type.loc['FA_Tim_75'], year_type.loc['FA_Tim_25']]
@@ -135,71 +175,99 @@ def hydrograph(ffc_data, rh_data):
     dsmed_y = year_type.loc['DS_Mag_50_med']
     wsmed_y = year_type.loc['Wet_BFL_Mag_50_med']
     # change peak value to lowest percentile for dry years
-    peakmed_y = year_type.loc['Peak_2_med']
+    if years == 'dry':
+        peakmed_y = year_type.loc['Peak_2_med']
+    else:
+        peakmed_y = year_type.loc['Peak_5_med']
     spmed_y = year_type.loc['SP_Mag_med']
     sp_rocmed_y = year_type.loc['SP_Mag_med']-30*year_type.loc['SP_ROC_med']*year_type.loc['SP_Mag_med']
     y_med = [dsmed_y, dsmed_y,famed_y,dsmed_y, dsmed_y,wsmed_y, wsmed_y,peakmed_y,wsmed_y, wsmed_y, spmed_y, sp_rocmed_y, dsmed_y, dsmed_y]
-    plt.plot(x_med, y_med, 'b--')
+    plt.plot(x_med, y_med, 'k--', zorder=4)
 
     '''
-    Plot 25/57 FFC hydrograph as filled in error bounds
+    Plot error bound fill to show 10-90 range of FFC values
     '''
-    # all x values corresponding with both 25th and 75th percentile hydrographs
 
-    ws75_x = year_type.loc['Wet_Tim_75']
-    ws25_x = year_type.loc['Wet_Tim_25']
-    pre_sp_75_x = year_type.loc['SP_Tim_75']-60
-    pre_sp_25_x = year_type.loc['SP_Tim_25']-60
-    sp75_x = year_type.loc['SP_Tim_75']
-    sp25_x = year_type.loc['SP_Tim_25']
-    sp_roc25_x = year_type.loc['SP_Tim_25']+30
-    sp_roc75_x = year_type.loc['SP_Tim_75']+30
-    ds25_x = year_type.loc['DS_Tim_25']
-    ds75_x = year_type.loc['DS_Tim_75']
-    # x_vals_fill = [0, ws75, ws75, ws25, ws25, pre_sp_75, pre_sp_25, sp75, sp25, sp_roc25, sp_roc75, ds25, ds75, 366]
-    x_25 = [0, ws25_x, ws25_x, pre_sp_25_x, sp25_x, sp_roc25_x, ds25_x]
-    x_75 = [0, ws75_x, ws75_x, pre_sp_75_x, sp75_x, sp_roc75_x, ds75_x]
+    ws_x_10, pre_sp_x_10, sp_x_10, sp_roc_x_10, ds_x_10, fa_y_10, ds_y_10, ws_y_10, sp_y_10, sp_roc_y_10 = define_fill_points(year_type, '10', spmed_y, sp_rocmed_y)
+    ws_x_25, pre_sp_x_25, sp_x_25, sp_roc_x_25, ds_x_25, fa_y_25, ds_y_25, ws_y_25, sp_y_25, sp_roc_y_25 = define_fill_points(year_type, '25', spmed_y, sp_rocmed_y)
+    ws_x_75, pre_sp_x_75, sp_x_75, sp_roc_x_75, ds_x_75, fa_y_75, ds_y_75, ws_y_75, sp_y_75, sp_roc_y_75 = define_fill_points(year_type, '75', spmed_y, sp_rocmed_y)
+    ws_x_90, pre_sp_x_90, sp_x_90, sp_roc_x_90, ds_x_90, fa_y_90, ds_y_90, ws_y_90, sp_y_90, sp_roc_y_90 = define_fill_points(year_type, '90', spmed_y, sp_rocmed_y)
 
-    # all 25th percentile fill y vals
-    fa25_y = year_type.loc['FA_Mag_25']
-    ds25_y = year_type.loc['DS_Mag_50_25']
-    ws25_y = year_type.loc['Wet_BFL_Mag_50_25']
-    sp25_y = year_type.loc['SP_Mag_25']
-    sp_roc25_y = sp_rocmed_y - (spmed_y - sp25_y)
-    # if end of spring recession drops too low based on original magnitude settings, reassign magnitude to dry season value
-    if sp_roc25_y < ds25_y:
-        sp_roc25_y = ds25_y
+    x_10 = [0, ws_x_10, ws_x_10, pre_sp_x_10, sp_x_10, sp_roc_x_10, ds_x_10]
+    x_25 = [0, ws_x_25, ws_x_25, pre_sp_x_25, sp_x_25, sp_roc_x_25, ds_x_25]
+    x_75 = [0, ws_x_75, ws_x_75, pre_sp_x_75, sp_x_75, sp_roc_x_75, ds_x_75]
+    x_90 = [0, ws_x_90, ws_x_90, pre_sp_x_90, sp_x_90, sp_roc_x_90, ds_x_90]
 
-    fa75_y = year_type.loc['FA_Mag_75']
-    ds75_y = year_type.loc['DS_Mag_50_75']
-    ws75_y = year_type.loc['Wet_BFL_Mag_50_75']
-    sp75_y = year_type.loc['SP_Mag_75']
-    sp_roc75_y = sp_rocmed_y + (sp75_y - spmed_y)
-    
-    y_25 = [ds25_y, ds25_y, ws25_y, ws25_y, sp25_y, sp_roc25_y, ds25_y]
-    y_75 = [ds75_y, ds75_y, ws75_y, ws75_y, sp75_y, sp_roc75_y, ds75_y]
+    y_10 = [ds_y_10, ds_y_10, ws_y_10, ws_y_10, sp_y_10, sp_roc_y_10, ds_y_10]
+    y_25 = [ds_y_25, ds_y_25, ws_y_25, ws_y_25, sp_y_25, sp_roc_y_25, ds_y_25]
+    y_75 = [ds_y_75, ds_y_75, ws_y_75, ws_y_75, sp_y_75, sp_roc_y_75, ds_y_75]
+    y_90 = [ds_y_90, ds_y_90, ws_y_90, ws_y_90, sp_y_90, sp_roc_y_90, ds_y_90]
+
 
     '''
     Use fill between function to plot 25/75 percentile bounds
     '''
     # fill from start of year to start of wet season, including fall pulse
-    plt.fill_between([0, famed_x-.5*fadur_x, famed_x, famed_x+.5*fadur_x, wsmed_x], [ds25_y, ds25_y, fa25_y, ds25_y, ds25_y], [ds75_y, ds75_y, fa75_y,ds75_y,ds75_y], facecolor='grey')
+    plt.fill_between([0, famed_x-.5*fadur_x, famed_x, famed_x+.5*fadur_x, wsmed_x], [ds_y_25, ds_y_25, fa_y_25, ds_y_25, ds_y_25], [ds_y_75, ds_y_75, fa_y_75,ds_y_75,ds_y_75], facecolor='blue', zorder=3, alpha=0.5)
     # vertical fill around wet season start (to capture variation in start timing)
-    plt.fill_betweenx([ds25_y, wsmed_y], [ws25_x], [ws75_x], facecolor='grey')
+    plt.fill_betweenx([ds_y_25, ws_y_75], [ws_x_25], [ws_x_75], facecolor='blue', zorder=3, alpha=0.75)
     # fill around wet season magnitude until start of spring
-    plt.fill_between([ws25_x, pre_spmed_x], [ws25_y], [ws75_y], facecolor='grey')
+    plt.fill_between([ws_x_75, pre_spmed_x], [ws_y_25], [ws_y_75], facecolor='blue', zorder=3, alpha=0.75)
     # fill around rising limb of spring, arbitrary starting point at 60 days before 
-    plt.fill_between([pre_spmed_x, spmed_x], [ws25_y, sp25_y], [ws75_y, sp75_y], facecolor='grey')
+    plt.fill_between([pre_spmed_x, spmed_x], [ws_y_25, sp_y_25], [ws_y_75, sp_y_75], facecolor='blue', zorder=3, alpha=0.75)
     # fill around falling limb of spring, chose 30 days to set SP_ROC slope (with adjustments)
-    plt.fill_between([spmed_x, sp_rocmed_x], [sp25_y, sp_roc25_y], [sp75_y, sp_roc75_y], facecolor='grey')
+    plt.fill_between([spmed_x, sp_rocmed_x], [sp_y_25, sp_roc_y_25], [sp_y_75, sp_roc_y_75], facecolor='blue', zorder=3, alpha=0.75)
     # fill around falling limb from spring ROC to start of dry season
-    plt.fill_between([sp_rocmed_x, dsmed_x], [sp_roc25_y, ds25_y], [sp_roc75_y, ds75_y], facecolor='grey')
+    plt.fill_between([sp_rocmed_x, dsmed_x], [sp_roc_y_25, ds_y_25], [sp_roc_y_75, ds_y_75], facecolor='blue', zorder=3, alpha=0.75)
     # fill from dry season to end of water year
-    plt.fill_between([dsmed_x, 366], [ds25_y], [ds75_y], facecolor='grey')
+    plt.fill_between([dsmed_x, 366], [ds_y_25], [ds_y_75], facecolor='blue', zorder=3, alpha=0.75)
+
+    '''
+    Use fill between function to plot 10/90 percentile bounds
+    '''
+    # fill from start of year to start of wet season, including fall pulse
+    plt.fill_between([0, famed_x-.5*fadur_x, famed_x, famed_x+.5*fadur_x, wsmed_x], [ds_y_10, ds_y_10, fa_y_10, ds_y_10, ds_y_10], [ds_y_90, ds_y_90, fa_y_90,ds_y_90,ds_y_90], facecolor='blue', zorder=2, alpha=0.65)
+    # vertical fill around wet season start (to capture variation in start timing)
+    plt.fill_betweenx([ds_y_10, ws_y_90], [ws_x_10], [ws_x_90], facecolor='blue', zorder=2, alpha=0.65)
+    # fill around wet season magnitude until start of spring
+    plt.fill_between([ws_x_90, pre_spmed_x], [ws_y_10], [ws_y_90], facecolor='blue', zorder=2, alpha=0.65)
+    # fill around rising limb of spring, arbitrary starting point at 60 days before 
+    plt.fill_between([pre_spmed_x, spmed_x], [ws_y_10, sp_y_10], [ws_y_90, sp_y_90], facecolor='blue', zorder=2, alpha=0.65)
+    # fill around falling limb of spring, chose 30 days to set SP_ROC slope (with adjustments)
+    plt.fill_between([spmed_x, sp_rocmed_x], [sp_y_10, sp_roc_y_10], [sp_y_90, sp_roc_y_90], facecolor='blue', zorder=2, alpha=0.65)
+    # fill around falling limb from spring ROC to start of dry season
+    plt.fill_between([sp_rocmed_x, dsmed_x], [sp_roc_y_10, ds_y_10], [sp_roc_y_90, ds_y_90], facecolor='blue', zorder=2, alpha=0.65)
+    # fill from dry season to end of water year
+    plt.fill_between([dsmed_x, 366], [ds_y_10], [ds_y_90], facecolor='blue', zorder=2, alpha=0.65)
 
     plt.show()
-    import pdb; pdb.set_trace()
-    # plt.plot((x1, x2), (y1, y2), 'k-')
     # import pdb; pdb.set_trace()
-    plt.show()
+
+    '''
+    Output table of hydrograph values
+    '''
+    year_types = ['all_years', 'dry', 'mod', 'wet']
+    output_table = pd.DataFrame(columns = ['val'])
+    metrics = [ 'DS_Mag_50', 'FA_Mag', 'Wet_Tim', 'Wet_BFL_Mag_50', 'Peak_5','Peak_Dur_5', 'SP_Tim', 'SP_Mag', 'SP_ROC', 'DS_Tim']
+    percentiles = [['_10', .10], ['_25', .25], ['_med', .5], ['_75', .75], ['_90', .9]]
+    for year_type in year_types:
+        for metric in metrics:
+            for label, quantile in percentiles:
+                if year_type == 'all_years':
+                    output_table.loc[metric+label, 'val'] = control_transpose[metric].quantile(quantile) 
+                elif year_type == 'dry':
+                    output_table.loc[metric+label, 'dry'] = dry_q[metric].quantile(quantile) 
+                elif year_type == 'mod':
+                    output_table.loc[metric+label, 'mod'] = mod_q[metric].quantile(quantile) 
+                elif year_type == 'wet':
+                    output_table.loc[metric+label, 'wet'] = wet_q[metric].quantile(quantile)
+        output_table.to_csv('hydrograph_metrics_'+year_type+'.csv')
+    import pdb; pdb.set_trace()
+
+
+
+
+
+
+
 
